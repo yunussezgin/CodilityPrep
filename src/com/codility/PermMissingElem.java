fa7785a9833a4073c31fc3b2519e79539516860d
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-02-12 15:41:51 +0300

    Perm Missing Element Problem

---
+++ src/com/codility/PermMissingElem.java
@@ -0,0 +1,15 @@
+package com.codility;
+
+public class PermMissingElem {
+
+	public int solution(int[] A) {
+		long actualSum = 0;
+		for (int number : A) {
+			actualSum += number;
+		}
+		long maxNumber = A.length + 1;
+		long expectedSum = (maxNumber * (maxNumber + 1) / 2);
+		return (int) (expectedSum - actualSum);
+	}
+
+}
