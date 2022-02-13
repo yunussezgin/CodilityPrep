8201e39137385c0b4ea0b0e97978f414c8c6b761
Yunus Sezgin <yunussezgin8@hotmail.com>
2022-02-13 12:22:06 +0300

    Counting Elements - Max Counters Solution

--- src/com/codility/Main.java
+++ src/com/codility/Main.java
@@ -14,8 +14,11 @@ public class Main {
 //		System.out.println(new TapeEquilibrium().solution(new int[] { 3, 1, 2, 4, 3 }));
 
 		// Cyclic Rotation
-		System.out.println(Arrays.toString(new CyclicRotation().solution(new int[] { 7, 2, 8, 3, 5 }, 2)));
-		System.out.println(Arrays.toString(new CyclicRotation().solution(new int[] { 7, 2, 8, 3, 5 }, 5)));
+//		System.out.println(Arrays.toString(new CyclicRotation().solution(new int[] { 7, 2, 8, 3, 5 }, 2)));
+//		System.out.println(Arrays.toString(new CyclicRotation().solution(new int[] { 7, 2, 8, 3, 5 }, 5)));
+
+		// Max Counters
+		System.out.println(Arrays.toString(new MaxCounters().solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 })));
 
 	}
 
---
+++ src/com/codility/MaxCounters.java
@@ -0,0 +1,34 @@
+package com.codility;
+
+import java.util.Arrays;
+
+public class MaxCounters {
+
+	public int[] solution(int N, int[] A) {
+		int[] counters = new int[N];
+		Arrays.fill(counters, 0);
+		int start_line = 0;
+		int current_max = 0;
+		for (int i : A) {
+			int x = i - 1;
+			if (i > N) {
+				start_line = current_max;
+			} else if (counters[x] < start_line) {
+				counters[x] = start_line + 1;
+			} else {
+				counters[x] += 1;
+			}
+			if (i <= N && counters[x] > current_max) {
+				current_max = counters[x];
+			}
+		}
+		for (int i = 0; i < counters.length; i++) {
+			if (counters[i] < start_line) {
+				counters[i] = start_line;
+			}
+		}
+
+		return counters;
+	}
+
+}
